43ab010c1d6705a23fc87d7a2b21f50e3a1f4d7f
sax <s.apostolico@gmail.com>
2014-07-15 10:12:10 +0200

    add sync-all-repo

---
+++ bin/sync-all-repo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+for dir in `find . -maxdepth 1 -type d  -printf '%f '`; do
+	pushd "${dir}" > /dev/null
+	if [ -d "$PWD/.git" ]; then
+		echo "Current dir: ${PWD}"
+		git fetch --all
+		git gc --prune=now
+		git remote prune origin
+		#git branch --merged | grep -v "\*" | xargs -n 1 git branch -d  # delete merged branches
+		git branch -a
+		for i in $(git branch | sed 's/^.//'); do
+			git checkout $i || exit 1
+			git pull && git push
+		done
+		git co develop # back to develop
+		#git pull --all || exit 1>
+		#git push --all  || exit 1
+		#git push --tags  || exit 1
+		echo "================================================================================================================================"
+	fi
+	popd > /dev/null
+done
+
